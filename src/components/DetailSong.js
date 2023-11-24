c8c10de477d011b4260e00959d8b7b132b40668f
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2023-11-25 02:43:23 +0700

    Spotify

---
+++ src/components/DetailSong.js
@@ -0,0 +1,7 @@
+function DetailSong() {
+    return ( 
+        <div className="col-span-1">1</div>
+     );
+}
+
+export default DetailSong;
---
+++ src/components/ListSongs.js
@@ -0,0 +1,7 @@
+function ListSongs() {
+    return (
+        <div className="col-span-2">2</div>
+    );
+}
+
+export default ListSongs;
