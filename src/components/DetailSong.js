f8b8875b30a0b3835769b552c29dfa389425c0cb
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2023-11-26 23:47:56 +0700

    Final Spotìy

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -1,14 +1,20 @@
+import { useContext } from 'react';
+import {Songs} from '../Context'
+
 function DetailSong() {
+
+    const {song} = useContext(Songs)
+
     return ( 
         <div className="col-span-1 p-3">
             <h2 className="text-[#3cbce6] font-bold">Now playing</h2>
-            <h2 className="text-[#868f9b] text-3xl">Sing me to sleep</h2>
+            <h2 className="text-[#868f9b] text-3xl">{song.name}</h2>
             <div className="w-[240px] m-auto mt-10">
-                <img className="w-[240px] h-[240px] object-cover" src='https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/403677110_1789919398122355_7339255759337431988_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=BZJhQgbbkQ8AX-X3mS6&_nc_ht=scontent.fhan17-1.fna&oh=00_AfCbulSNuqivVzJMJHL8nCiMqE5KznBRyLl_xYhQfo6Ddw&oe=6566DC26' alt="avatar"/>
+                <img className="w-[240px] h-[240px] object-cover" src={song.links.images[0].url} alt="avatar"/>
             </div>
             <div className="flex justify-evenly items-center mt-10">
-                <img className="w-[70px] h-[70px] object-cover rounded-full" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/404679377_1789152041532424_4179965259806865108_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=r72ExhFRP0oAX_T9rC1&_nc_ht=scontent.fhan17-1.fna&oh=00_AfBeUQscwWVOtKFVvu3Rzv_68vGOUmrw1rzJJ28wiLc5FA&oe=65686B66" alt="avatar"/>
-                <span className="text-xl text-[#fff]">Alan Walker</span>
+                <img className="w-[70px] h-[70px] object-cover rounded-full" src={song.links.images[1].url} alt="avatar"/>
+                <span className="text-xl text-[#fff]">{song.author}</span>
             </div>
         </div>
      );
--- src/components/ListSongs.js
+++ src/components/ListSongs.js
@@ -1,14 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Songs } from '../Context'
 
 function ListSongs() {
     
-    const {DataSongs} = useContext(Songs)
-    console.log(DataSongs);
+    const {DataSongs, handleSetSong, song} = useContext(Songs)
+    const [idSong, setIdSong] = useState(0)
 
+    const handlePlaySong = (idSong) => {
+        setIdSong(idSong)
+        handleSetSong(idSong)
+    }
+
+    useEffect(() => {
+        setIdSong(song.id)
+    }, [song])
     return (
-        <div className="col-span-2">
-            <table className="table-auto w-full">
+        <div className="col-span-2 overflow-y-scroll">
+            <table className="table-auto w-full ">
                 <thead className="text-[#fff] h-12">
                     <tr>
                         <th className="w-[10%]">#</th>
@@ -20,7 +28,9 @@ function ListSongs() {
                 <tbody>
                     {
                         DataSongs.map((song, index) => (
-                            <tr key={index} className="bg-slate-800 h-12 text-gray-500 hover:bg-slate-400 hover:cursor-pointer">
+                            <tr key={index} 
+                            className={`bg-slate-800 h-12 text-gray-500 hover:bg-slate-400 hover:cursor-pointer hover:text-[#fff] ${idSong === song.id && 'text-teal-400 bg-slate-400'}`} 
+                            onClick={() => handlePlaySong(song.id)}>
                                 <td className="text-center">{index + 1}</td>
                                 <td >{song.name}</td>
                                 <td className="text-center">{song.author}</td>
