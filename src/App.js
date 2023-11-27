b98391cdd3203a750e1638f562cee8ce553e2708
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2023-11-28 04:41:54 +0700

    Thêm chú thích Spotifyy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css'
 import DetailSong from "./components/DetailSong";
 import ListSongs from "./components/ListSongs";
 import Navbar from "./components/Navbar";
-import { Songs } from "./Context";
-import DataSongs from './data/songs.json'
 import Playing from './components/Playing'
+
+import DataSongs from './data/songs.json'
+import { Songs } from "./Context";
 import { useState } from 'react';
 
 function App() {
@@ -12,13 +13,30 @@ function App() {
   const [song, setSong] = useState(DataSongs[0])
 
   const handleSetSong = (idSong) => {
+    // Ở đây tìm kiếm nếu song.id trong DataSongs trùng với idSong thì lưu vào biến song
     const song = DataSongs.find(song => song.id === idSong)
     if(!song){
       setSong(DataSongs[0])
     } else {
       setSong(song)
+      // setSong đã cập nhật một state mới là song(trong handleSetSong) sau đó song(trong handleSetSong) này đi lên thành giá trị của song state
     }
   }
+
+  // //Như này sẽ dễ hiểu hơn nha!
+// const [currentSong, setCurrentSong] = useState(DataSongs[0]);
+
+// const handleSetSong = (idSong) => {
+//   const foundSong = DataSongs.find(song => song.id === idSong);
+
+//   if (!foundSong) {
+//     setCurrentSong(DataSongs[0]);
+//   } else {
+//     // Nếu tìm thấy, cập nhật currentSong với bài hát tìm được
+//     setCurrentSong(foundSong);
+//   }
+// };
+
 
   return (
     <div className="App">
--- src/Context.js
+++ src/Context.js
@@ -1,3 +1,5 @@
-import React from "react";
+import React from "react"; //Từ phiền bản 16.03 trở đi thì không cần phải import createContext từ react
 
+// Đối tượng Context được tạo bằng React.createContext() cho phép bạn chia sẻ dữ liệu global 
+// (hoặc giữa các thành phần không có mối quan hệ cha con trực tiếp) một cách thuận tiện và hiệu quả.
 export const Songs = React.createContext()
